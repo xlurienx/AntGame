f1bc76f58f81f4dd2940a81ead11189e3b13e015
xlurienx <emilytheresa0223@gmail.com>
2025-02-25 19:28:54 -0500

    g

--- Assets/ant.cs
+++ Assets/ant.cs
@@ -1,61 +1,13 @@
 using UnityEngine;
 
-[System.Serializable]
-public class Enemy
-{
-    public string Name;
-    public GameObject ItemPrefab;
-    [Range (0f, 100f)]public float Chance = 100f;
 
-    [HideInInspector] public double _weight;
-
-}
 
 public class ant : MonoBehaviour
 {
 [SerializeField]
 public GameObject[] ItemPrefab;
 public float Timer = 1;
-
-    private double accumulatedWeights;
-    private System.Random rand = new System.Random();
-
-    private void Awake()
-    {
-        CalculateWeights ();
-    }
-    private void Start()
-    {
-        for (int i = 0; i < 0; i++)
-            SpawnRandomEnemy(new Vector2(Random.Range(-3f, 3f), Random.Range(-4f, 4f)));
-    }
-
-    private void SpawnRandomEnemy (Vector2 position)
-    {
-        Enemy randomEnemy = ItemPrefab [GetRandomEnemyIndex()];
-
-        Instantiate(randomEnemy.ItemPrefab, position, Quaternion.identity, transform);
-    }
-
-    private int GetRandomEnemyIndex()
-    {
-        double r = rand.NextDouble() * accumulatedWeights;
-        for (int i = 0; i < ItemPrefab.Length; i++)
-            if (ItemPrefab[i].weight >= r)
-                return i;
-        return 0;
-        
-    }
-    private void CalculateWeights()
-    {
-        accumulatedWeights = 0f;
-        foreach (Enemy ItemPrefab in ItemPrefab)
-        {
-            accumulatedWeights += ItemPrefab.Chance;
-            ItemPrefab._weight = accumulatedWeights;
-
-        }
-    }
+    
     void Update()
     {
         Timer -= Time.deltaTime;
